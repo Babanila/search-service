e834c9721e3d1e178a36a1a937caf35c6b482a24
Babanila <babanila@yahoo.com>
2020-06-15 00:18:29 +0200

    refactor(): code refactor

--- server.js
+++ server.js
@@ -1,44 +1,12 @@
 const express = require("express");
-const got = require("got");
-const { formatLanguages, getUserDetails } = require("./src/helpers");
+const { getUser } = require("./src/helpers");
 
 const app = express();
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
-app.get("/", (req, res) => res.send(`Please check the READ.me file`));
-
-app.get("/get-users", async (req, res) => {
-  const { username, language } = req.query;
-  const formatedLanguage = formatLanguages(language);
-  try {
-    const response = await got(
-      `https://api.github.com/search/users?q=${username}+in:login${formatedLanguage}`
-    );
-    const { body } = response;
-    if (!body || body.length === 0) {
-      res
-        .status(200)
-        .send(`No Github user with the programming language ${language}`);
-    }
-    const rest = JSON.parse(body);
-    const formatted = getUserDetails(
-      rest.items,
-      `https://api.github.com/users`
-    );
-
-    return Promise.all(formatted)
-      .then((final) => res.status(200).send(final))
-      .catch((_) => res.status(403).send("Rate Limit Exceeded"));
-  } catch (error) {
-    if (error.name === "HTTPError") {
-      res.status(403).send("Rate limit exceeded");
-      res.status(500).send("Internal Server Error");
-    } else {
-      throw error;
-    }
-  }
-});
+app.get("/", (req, res) => res.send(`Please check the README file`));
+app.get("/get-users", getUser);
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}/`);
--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,67 @@
 const got = require("got");
+const baseUrl = "https://api.github.com";
+const timeout = 20000;
 
-const formatLanguages = (languageInput) => {
+const formatLanguages = (languageInput = "") => {
   const languageArray = languageInput.split(",");
-  let formatLanguage = "";
-  for (let i = 0; i < languageArray.length; i++) {
-    formatLanguage += `+language:"${languageArray[i]}"`;
-  }
-  return formatLanguage;
-};
-const getUserDetails = (arrayOfUsers, userUrl) => {
-  return arrayOfUsers.map(({ login, avatar_url }) => {
-    return got(`${userUrl}/${login}`).then((user) => {
-      const { followers, name } = JSON.parse(user.body);
-      return { name, followers, avatar_url, username: login };
-    });
-  });
+  return languageArray.map((item) => `language:"${item}"`);
 };
 
-module.exports = { formatLanguages, getUserDetails };
+const fetcher = (url) =>
+  got(url, { timeout })
+    .then((res) => JSON.parse(res.body))
+    .catch((e) => {
+      if (e.name === "TimeoutError") return "Timeout";
+      throw e;
+    });
+
+const getUserDetails = (arrayOfUsers, userUrl) =>
+  arrayOfUsers.map(({ login, avatar_url }) =>
+    fetcher(`${userUrl}/${login}`).then(({ followers, name }) => ({
+      name,
+      followers,
+      avatar_url,
+      username: login,
+    }))
+  );
+
+const getUser = async (req, res) => {
+  const { username, language } = req.query;
+  const formatedLanguages = formatLanguages(language);
+  let searchResults;
+  try {
+    for (const formatedLanguage of formatedLanguages) {
+      const response = await fetcher(
+        `${baseUrl}/search/users?q=${username}+in:login+${formatedLanguage}`
+      );
+      if (response === "Timeout") {
+        continue;
+      }
+      if (response.items.length) {
+        searchResults = response.items;
+        break;
+      }
+    }
+    if (!searchResults.length) {
+      res
+        .status(200)
+        .send(`No Github user with the programming language ${language}`);
+      return;
+    }
+    const userDetailsPromises = getUserDetails(
+      searchResults,
+      `${baseUrl}/users`
+    );
+
+    const final = await Promise.all(userDetailsPromises);
+    res.status(200).send(final);
+  } catch (error) {
+    if (error.status === 403) {
+      res.status(400).send("Rate Limit Exceeded, Try Again later");
+    } else {
+      res.status(500).send("Internal Server Error");
+    }
+  }
+};
+
+module.exports = { formatLanguages, getUserDetails, getUser, fetcher };
