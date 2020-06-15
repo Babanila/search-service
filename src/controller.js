2ce9fa5e3f1ec52ad512cfbe7aed6e5ba534e859
Babanila <babanila@yahoo.com>
2020-06-15 20:00:35 +0200

    test(): add utils test

--- src/controller.js
+++ src/controller.js
@@ -17,7 +17,25 @@ module.exports = function createController(
 
   const getUsers = async (req, res) => {
     const { username, language } = req.query;
+
+    if (!language) {
+      return res
+        .status(200)
+        .send(
+          `Please add value for the language parameter in the query. E.g ../get-users?username=john&language=java`
+        );
+    }
+
     const formatedLanguages = formatLanguages(language);
+
+    if (formatedLanguages.length > 5) {
+      return res
+        .status(200)
+        .send(
+          `Please reduce the number of programming languages parameter in the query, maximum of 5 is allowed.`
+        );
+    }
+
     let searchResults;
     try {
       for (const formatedLanguage of formatedLanguages) {
@@ -28,16 +46,16 @@ module.exports = function createController(
         if (response === "Timeout") {
           continue;
         }
-        if (response.items.length) {
+        if (response.items.length !== 0) {
           searchResults = response.items;
           break;
         }
       }
-      if (!searchResults.length) {
-        res
+
+      if (!searchResults) {
+        return res
           .status(200)
           .send(`No Github user with the programming language ${language}`);
-        return;
       }
 
       const userDetails = await PromiseMap(
@@ -57,12 +75,12 @@ module.exports = function createController(
         { concurrency: 5 }
       );
 
-      res.status(200).send(userDetails);
+      return res.status(200).send(userDetails);
     } catch (error) {
       if (error.status === 403) {
-        res.status(400).send("Rate Limit Exceeded, Try Again later");
+        return res.status(400).send("Rate Limit Exceeded, Try Again later");
       } else {
-        res.status(500).send("Internal Server Error");
+        return res.status(500).send("Internal Server Error");
       }
     }
   };
